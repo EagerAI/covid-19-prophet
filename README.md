f3e1d3b866e372051697703da30f0eac4efe19df
Turgut Abdullayev <turqut.a.314@gmail.com>
2020-12-01 21:58:22 +0400

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# covid-19-prophet
+# Covid-19-prophet
+
+
+We use ```modeltime``` package to predict the new Covid-19 cases for the next months.
+
+
+
--- rscript.R
+++ rscript.R
@@ -3,6 +3,6 @@ if(!dir.exists('docs')) {
   dir.create('docs')
 }
 
-rmarkdown:::render('covid_dash.Rmd',output_dir = 'docs',output_file = 'index.html')
+rmarkdown:::render('covid.Rmd',output_dir = 'docs',output_file = 'index.html')
 
 
